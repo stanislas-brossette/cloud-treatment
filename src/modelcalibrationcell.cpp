6111451c20d00c540aed6686c172a266fd38b0fa
Stanislas Brossette <brossette@lirmm.fr>
2013-05-01 18:19:03 +0900

    Implemented the correspondence grouping algorithm and the visualization of the results. First interesting results of model calibration

--- src/modelcalibrationcell.cpp
+++ src/modelcalibrationcell.cpp
@@ -11,8 +11,13 @@
 #include <eigen3/Eigen/Dense>
 
 #include <pcl/apps/render_views_tesselated_sphere.h>
+#include <pcl/common/transforms.h>
+#include <pcl/correspondence.h>
 #include <pcl/features/normal_3d.h>
 #include <pcl/io/pcd_io.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/kdtree/impl/kdtree_flann.hpp>
+#include <pcl/recognition/cg/geometric_consistency.h>
 #include <pcl/search/kdtree.h>
 #include <pcl/keypoints/uniform_sampling.h>
 #include <pcl/visualization/pcl_visualizer.h>
@@ -30,28 +35,70 @@
 ModelCalibrationCell::ModelCalibrationCell():
 	Cell("ModelCalibrationCell"),
 	views_(),
+	views_normals_(),
+	views_keypoints_(),
+	views_descriptors_(),
+	model_scene_corrs_(),
 	uniform_sampling_(),
-	descr_est_()
+	descr_est_(),
+	tree_(boost::shared_ptr<pcl::search::Search<pcl::PointXYZ> >
+		  (new pcl::search::KdTree<pcl::PointXYZ>)),
+	normal_estimator_()
+
 {
+	parameters()["views_resolution"] = 100;
 	parameters()["number_of_neighbours_normal_estimation"] = 10;
 	parameters()["keypoint_search_radius_scene"] = 0.1f;
 	parameters()["descriptor_search_radius_scene"] = 0.1f;
-	std::string cadModelFile = "chair.ply";
-
-//	generateViewsFromCADModelFile(cadModelFile);
-//	std::cout << std::endl << "Generated " << views_.size() << " views of " << cadModelFile << std::endl;
-
-
+	parameters()["keypoint_search_radius_model"] = 0.1f;
+	parameters()["descriptor_search_radius_model"] = 0.1f;
+	parameters()["correspondence_grouping_size"] = 0.05f;
+	parameters()["correspondence_grouping_threshhold"] = 5;
 }
 
 planCloudsPtr_t ModelCalibrationCell::compute(planCloudsPtr_t planCloudListPtr)
 {
+	views_resolution_ =
+			static_cast<int>(parameters()["views_resolution"]);
 	number_of_neighbours_normal_estimation_ =
 			static_cast<int>(parameters()["number_of_neighbours_normal_estimation"]);
 	keypoint_search_radius_scene_ =
 			static_cast<float>(parameters()["keypoint_search_radius_scene"]);
 	descriptor_search_radius_scene_ =
 			static_cast<float>(parameters()["descriptor_search_radius_scene"]);
+	keypoint_search_radius_model_ =
+			static_cast<float>(parameters()["keypoint_search_radius_model"]);
+	descriptor_search_radius_model_ =
+			static_cast<float>(parameters()["descriptor_search_radius_model"]);
+	correspondence_grouping_size_ =
+			static_cast<float>(parameters()["correspondence_grouping_size"]);
+	correspondence_grouping_threshhold_ =
+			static_cast<int>(parameters()["correspondence_grouping_threshhold"]);
+
+	std::string cadModelFile = "chair_remeshed.ply";
+
+	generateViewsFromCADModelFile(cadModelFile);
+	std::cout << std::endl << "Generated " << views_.size() << " views of " << cadModelFile << std::endl;
+
+	views_normals_.resize(views_.size());
+	views_keypoints_.resize(views_.size());
+	views_descriptors_.resize(views_.size());
+	model_scene_corrs_.resize(views_.size());
+
+	for(std::size_t i = 0; i < views_.size(); ++i)
+	{
+		views_normals_[i] = computeNormals(views_[i]);
+		views_keypoints_[i] = computeKeypoints(views_[i], keypoint_search_radius_model_);
+		views_descriptors_[i] = computeDescriptors(
+					views_[i],
+					views_normals_[i],
+					views_keypoints_[i],
+					descriptor_search_radius_model_);
+		std::cout<<"views_["<<i<<"] contains " << views_[i]->size() << " points\n"
+				<<views_normals_[i]->size() << " normals\n"
+				<<views_keypoints_[i]->size() << " keypoints\n"
+				<<views_descriptors_[i]->size() << "descriptors \n\n";
+	}
 
 	for(pointCloudPoints_t::size_type j = 0; j<planCloudListPtr->size(); ++j)
 	{
@@ -67,13 +114,9 @@ planCloudsPtr_t ModelCalibrationCell::compute(planCloudsPtr_t planCloudListPtr)
 		planCloudListPtr->at(j).keyPoints() = computeKeypoints(
 					planCloudListPtr->at(j).cloud(), keypoint_search_radius_scene_);
 
-		std::cout << "Scene total points: " << planCloudListPtr->at(j).cloud()->size ()
-				  << "; Selected Keypoints: " << planCloudListPtr->at(j).keyPoints()->size () << std::endl;
-
 		//
 		// Compute Descriptor for keypoints
 		//
-
 		planCloudListPtr->at(j).descriptors() = computeDescriptors(
 					planCloudListPtr->at(j).cloud(),
 					planCloudListPtr->at(j).normals(),
@@ -81,15 +124,134 @@ planCloudsPtr_t ModelCalibrationCell::compute(planCloudsPtr_t planCloudListPtr)
 					descriptor_search_radius_scene_);
 
 
-		std::cout << "Scene total points: " << planCloudListPtr->at(j).cloud()->size ()
-				  << "; Selected Keypoints: " << planCloudListPtr->at(j).keyPoints()->size ()
-				  << "; Descriptors: " << planCloudListPtr->at(j).descriptors()->size ()
-				  << std::endl;
+		std::cout<<"Scene total points: " << planCloudListPtr->at(j).cloud()->size() << " points\n"
+				<<planCloudListPtr->at(j).normals()->size() << " normals\n"
+				<<planCloudListPtr->at(j).keyPoints()->size() << " keypoints\n"
+				<<planCloudListPtr->at(j).descriptors()->size() << "descriptors \n\n";
 
 		//
 		// Find Model-Scene Correspondences with KdTree
 		//
 
+		std::size_t best_view = 0;
+		std::size_t max_correspondences = 0;
+
+		for(std::size_t k = 0; k < views_.size(); ++k)
+		{
+			model_scene_corrs_[k] = boost::make_shared <pcl::Correspondences> ();
+			pcl::KdTreeFLANN<DescriptorType> match_search;
+			match_search.setInputCloud (views_descriptors_[k]);
+
+			//  For each scene keypoint descriptor, find nearest neighbor into the model
+			// keypoints descriptor cloud and add it to the correspondences vector.
+			for (size_t i = 0; i < planCloudListPtr->at(j).descriptors()->size (); ++i)
+			{
+				std::vector<int> neigh_indices (1);
+				std::vector<float> neigh_sqr_dists (1);
+				if (!pcl_isfinite (planCloudListPtr->at(j).descriptors()->at (i).descriptor[0])) //skipping NaNs
+				{
+					continue;
+				}
+				int found_neighs = match_search.nearestKSearch (
+							planCloudListPtr->at(j).descriptors()->at (i), 1,
+							neigh_indices, neigh_sqr_dists);
+				//  add match only if the squared descriptor distance is less than 0.25
+				// (SHOT descriptor distances are between 0 and 1 by design)
+				if(found_neighs == 1 && neigh_sqr_dists[0] < 0.25f)
+				{
+					pcl::Correspondence corr (neigh_indices[0], static_cast<int> (i),
+											  neigh_sqr_dists[0]);
+					model_scene_corrs_[k]->push_back (corr);
+				}
+			}
+			std::cout << "Correspondences with view_[" << k << "] found: " << model_scene_corrs_[k]->size () << std::endl;
+
+			// For now, we considere that the best view is the one that has the highest
+			// number of correspondences
+			// TO BE IMPROVED
+			if(model_scene_corrs_[k]->size () > max_correspondences)
+			{
+				max_correspondences = model_scene_corrs_[k]->size ();
+				best_view = k;
+			}
+		}
+		std::cout << "Best view: " << best_view << " with " << max_correspondences << " correspondences.\n";
+
+		//
+		//  Actual Clustering
+		//
+		std::vector<Eigen::Matrix4f, Eigen::aligned_allocator<Eigen::Matrix4f> > rototranslations;
+		std::vector<pcl::Correspondences> clustered_corrs;
+
+		pcl::GeometricConsistencyGrouping<pcl::PointXYZ, pcl::PointXYZ> gc_clusterer;
+		gc_clusterer.setGCSize (correspondence_grouping_size_);
+		gc_clusterer.setGCThreshold (correspondence_grouping_threshhold_);
+
+		gc_clusterer.setInputCloud (views_keypoints_[best_view]);
+		gc_clusterer.setSceneCloud (planCloudListPtr->at(j).keyPoints());
+		gc_clusterer.setModelSceneCorrespondences (model_scene_corrs_[best_view]);
+
+		//gc_clusterer.cluster (clustered_corrs);
+		gc_clusterer.recognize (rototranslations, clustered_corrs);
+
+		std::cout << "Model instances found: " << rototranslations.size () << std::endl;
+		for (size_t i = 0; i < rototranslations.size (); ++i)
+		{
+			std::cout << "\n    Instance " << i + 1 << ":" << std::endl;
+			std::cout << "        Correspondences belonging to this instance: " << clustered_corrs[i].size () << std::endl;
+
+			// Print the rotation matrix and translation vector
+			Eigen::Matrix3f rotation = rototranslations[i].block<3,3>(0, 0);
+			Eigen::Vector3f translation = rototranslations[i].block<3,1>(0, 3);
+
+			printf ("\n");
+			printf ("            | %6.3f %6.3f %6.3f | \n", rotation (0,0), rotation (0,1), rotation (0,2));
+			printf ("        R = | %6.3f %6.3f %6.3f | \n", rotation (1,0), rotation (1,1), rotation (1,2));
+			printf ("            | %6.3f %6.3f %6.3f | \n", rotation (2,0), rotation (2,1), rotation (2,2));
+			printf ("\n");
+			printf ("        t = < %0.3f, %0.3f, %0.3f >\n", translation (0), translation (1), translation (2));
+		}
+
+
+
+		//Dummy visualization
+		boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
+		viewer->setBackgroundColor (255, 255, 255);
+		viewer->initCameraParameters ();
+
+		pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> scene_color(planCloudListPtr->at(j).cloud(), 140, 138, 134);
+		viewer->addPointCloud<pcl::PointXYZ> (planCloudListPtr->at(j).cloud(), scene_color, "Scene");
+		viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1.5, "Scene");
+
+		pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> sceneKey_color(planCloudListPtr->at(j).keyPoints(), 0, 0, 255);
+		viewer->addPointCloud<pcl::PointXYZ> (planCloudListPtr->at(j).keyPoints(), sceneKey_color, "SceneKey");
+		viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 4, "SceneKey");
+
+		pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> model_color(views_[best_view], 0, 138, 134);
+		viewer->addPointCloud<pcl::PointXYZ> (views_[best_view], model_color, "Model");
+		viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1.5, "Model");
+
+		pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> modelKey_color(views_keypoints_[best_view], 0, 255, 0);
+		viewer->addPointCloud<pcl::PointXYZ> (views_keypoints_[best_view], modelKey_color, "ModelKey");
+		viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 4, "ModelKey");
+
+		for (size_t i = 0; i < rototranslations.size (); ++i)
+		{
+			pcl::PointCloud<pcl::PointXYZ>::Ptr rotated_model = boost::make_shared<pcl::PointCloud<pcl::PointXYZ> > ();
+			pcl::transformPointCloud (*(views_[best_view]), *rotated_model, rototranslations[i]);
+
+			std::stringstream ss_cloud;
+			ss_cloud << "instance" << i;
+
+			pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> rotated_model_color_handler (rotated_model, 255, 0, 0);
+			viewer->addPointCloud<pcl::PointXYZ> (rotated_model, rotated_model_color_handler, ss_cloud.str ());
+		}
+
+		while (!viewer->wasStopped ())
+		{
+			viewer->spinOnce (100);
+			boost::this_thread::sleep (boost::posix_time::microseconds (10000));
+		}
 	}
 
 	return planCloudListPtr;
@@ -99,14 +261,10 @@ normalCloudPtr_t ModelCalibrationCell::computeNormals(
 		const pointCloudPtr_t& point_cloud_ptr_)
 {
 	normalCloudPtr_t normals = boost::make_shared<normalCloud_t> ();
-	pcl::search::Search<pcl::PointXYZ>::Ptr tree =
-			boost::shared_ptr<pcl::search::Search<pcl::PointXYZ> >
-			(new pcl::search::KdTree<pcl::PointXYZ>);
-	pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> normal_estimator;
-	normal_estimator.setSearchMethod (tree);
-	normal_estimator.setInputCloud (point_cloud_ptr_);
-	normal_estimator.setKSearch (number_of_neighbours_normal_estimation_);
-	normal_estimator.compute (*normals);
+	normal_estimator_.setSearchMethod (tree_);
+	normal_estimator_.setInputCloud (point_cloud_ptr_);
+	normal_estimator_.setKSearch (number_of_neighbours_normal_estimation_);
+	normal_estimator_.compute (*normals);
 
 	return normals;
 }
@@ -152,7 +310,7 @@ void ModelCalibrationCell::generateViewsFromCADModelFile(std::string cadModelFil
 	vtkSmartPointer< vtkPolyData > polydata = vtkSmartPointer< vtkPolyData > (mapper->GetInput ());
 
 	pcl::apps::RenderViewsTesselatedSphere render_views;
-	render_views.setResolution(100);
+	render_views.setResolution(views_resolution_);
 
 	render_views.addModelFromPolyData (polydata);
 
--- src/modelcalibrationcell.h
+++ src/modelcalibrationcell.h
@@ -3,6 +3,7 @@
 
 #include <boost/filesystem/path.hpp>
 
+#include <pcl/correspondence.h>
 #include <pcl/keypoints/uniform_sampling.h>
 #include <pcl/features/shot_omp.h>
 
@@ -36,12 +37,24 @@ private:
 			const float& search_radius);
 
 	std::vector< pointCloudPtr_t > views_;
+	std::vector< normalCloudPtr_t > views_normals_;
+	std::vector< pointCloudPtr_t > views_keypoints_;
+	std::vector< descriptorCloudPtr_t > views_descriptors_;
+	std::vector< pcl::CorrespondencesPtr > model_scene_corrs_;
+
 	pcl::UniformSampling<pcl::PointXYZ> uniform_sampling_;
 	pcl::SHOTEstimationOMP<pcl::PointXYZ, pcl::Normal, DescriptorType> descr_est_;
+	pcl::search::Search<pcl::PointXYZ>::Ptr tree_;
+	pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> normal_estimator_;
 
+	int views_resolution_;
 	int number_of_neighbours_normal_estimation_;
 	float keypoint_search_radius_scene_;
 	float descriptor_search_radius_scene_;
+	float keypoint_search_radius_model_;
+	float descriptor_search_radius_model_;
+	float correspondence_grouping_size_;
+	int correspondence_grouping_threshhold_;
 };
 
 #endif // MODELCALIBRATIONCELL_H
